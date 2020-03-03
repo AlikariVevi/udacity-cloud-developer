9ceb40eb542f28653849a0abb022db1293c28ad3
Paraskevi Alikari <par.alikari@gmail.com>
2020-03-03 21:21:15 +0100

    Add files via upload
    trying to connect with s3

--- udacity-c2-restapi/src/config/config.ts
+++ udacity-c2-restapi/src/config/config.ts
@@ -5,9 +5,9 @@ export const config = {
     "database": "udagramalikaride",
     "host": "udagramalikaride.czzxayxy6qse.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": "us-east-1",
+    "aws_profile": "C:\Users\Vevi\Desktop\WebDevop\AWS\credentials.csv",
+    "aws_media_bucket": 'udagram-alikari-dev'
   },
   "prod": {
     "username": "",
