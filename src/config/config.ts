975a37c3039c86860c0ef53b32a0264612f4f1af
Paraskevi Alikari <par.alikari@gmail.com>
2020-03-03 21:15:08 +0100

    Add files via upload

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramalikaride",
+    "password": "udagramalikaride",
+    "database": "udagramalikaride",
+    "host": "udagramalikaride.czzxayxy6qse.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": 'udagram-alikari-dev'
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
